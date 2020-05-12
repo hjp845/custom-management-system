d3438281fa440f12f6bbaa4fa04b27e4cb2343bf
Hwang Jung Pyeong <47222888+hjp845@users.noreply.github.com>
2020-05-13 03:48:11 +0900

    By map, several Users

--- src/App.js
+++ src/App.js
@@ -3,26 +3,53 @@ import logo from './logo.svg';
 import Customer from './components/Customer'
 import './App.css';
 
-const customer = {
+const customer = [
+  {
   'id': 1,
-  'image' : 'https://placeimg.com/64/64/any',
+  'image' : 'https://placeimg.com/64/64/1',
   'name' : '포스트말론',
   'birthday' : '951010',
   'gender' : 'man',
   'job' : 'singer'
-}
+},
+{
+  'id': 2,
+  'image' : 'https://placeimg.com/64/64/2',
+  'name' : '황정평',
+  'birthday' : '981025',
+  'gender' : 'man',
+  'job' : 'investor'
+},
+{
+  'id': 3,
+  'image' : 'https://placeimg.com/64/64/3',
+  'name' : '홍길동이',
+  'birthday' : '130134',
+  'gender' : 'man',
+  'job' : 'programmer'
+},
+]
 
 class App extends Component {
   render() {
     return (
-      <Customer
-        id={customer.id}
-        image={customer.image}
-        name={customer.name}
-        birthday={customer.birthday}
-        gneder={customer.gender}
-        job={customer.job}
-      />
+      <div>
+        {
+          customer.map(c => {
+            return (
+              <Customer
+              // key 는 map 에 있어서 그냥 필요한 값이다.
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              job={c.job}
+              />
+            )
+          })
+        }
+      </div>
     );
   }
 }
