bfb4f6b3acf46a1b39b19a092c88a5901017558a
Hwang Jung Pyeong <47222888+hjp845@users.noreply.github.com>
2020-05-12 22:57:31 +0900

    Create React Proj

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="gray-background">
+          <img src={logo} lat="logo"/>
+          <h2>Let's study managin system!</h2>
+          </div> 
+      </header>
+    </div>
+  );
+}
+
+export default App;
