7966037efeda9b851e965b13e40c15b6b09ad017
Hwang Jung Pyeong <47222888+hjp845@users.noreply.github.com>
2020-05-19 04:18:16 +0900

    Create Customer Database Table & Node.js Database Connection Settings

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,33 +7,26 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-        'id': 1,
-        'image' : 'https://placeimg.com/64/64/1',
-        'name' : '포스트말론',
-        'birthday' : '951010',
-        'gender' : 'man',
-        'job' : 'singer'
-      },
-      {
-        'id': 2,
-        'image' : 'https://placeimg.com/64/64/2',
-        'name' : '황정평',
-        'birthday' : '981025',
-        'gender' : 'man',
-        'job' : 'investor'
-      },
-      {
-        'id': 3,
-        'image' : 'https://placeimg.com/64/64/3',
-        'name' : '홍길동이',
-        'birthday' : '130134',
-        'gender' : 'man',
-        'job' : 'programmer'
-      },
-      ]);
+    connection.query(
+      "SELECT * FROM CUSTOMER",
+      (err, rows, fields) => {
+        res.send(rows);
+      }
+    );
 });
 
 // ` 러 헤줘야 문자열 안에 변수를 잘 출력할 수 잇음
