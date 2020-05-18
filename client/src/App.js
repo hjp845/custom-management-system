f4a62d692e255e790737e0503b2d62bee7c100a3
Hwang Jung Pyeong <47222888+hjp845@users.noreply.github.com>
2020-05-19 00:46:00 +0900

    React LifeCycle + Loading 처리 구현

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
@@ -18,18 +19,40 @@ const styles = theme => ({
   },
   table: {
     minWidth: 1080
-  }
+  },
+  progress: {
+    margin: theme.spacing.unit * 2
+  },
 })
 
+/*
 
+1) comstructor()
+
+2) componentWillMount()
+
+3) render()
+
+4) componentDidMount()
+
+*/
+/*
+
+바뀌면 알아서 업데이트
+props or state => sholudComponentUpdate()
+
+*/
 
 class App extends Component {
 
   state = {
-    customers: ""
+    customers: "",
+    completed: 0,
   }
 
+  // api 를 비동기적으로 호출
   componentDidMount() {
+    this.timer = setInterval(this.progress, 20);
     this.callApi()
       .then(res => this.setState({customer: res}))
       .catch(err => console.log(err));
@@ -39,6 +62,11 @@ class App extends Component {
     const response = await fetch('/api/customers');
     const body = await response.json();
     return body;
+  }
+
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({ completed: completed >= 100 ? 0 : completed + 1})
   }
 
   render() {
@@ -58,7 +86,13 @@ class App extends Component {
           </TableHead>
           <TableBody>
           {this.state.customer ? this.state.customer.map(c => {return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} job={c.job}/>)}) 
-          : ""}
+          : 
+          <TableRow>
+            <TableCell colSpan="6" align="center">
+              <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+            </TableCell>
+          </TableRow>
+          }
           </TableBody>
         </Table>        
       </Paper>
