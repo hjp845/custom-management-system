7f3df5dbc002d1a5653e9f5f24640ffeb40db710
Hwang Jung Pyeong <47222888+hjp845@users.noreply.github.com>
2020-05-26 21:33:36 +0900

    Design + 검색 기능

--- server.js
+++ server.js
@@ -12,35 +12,30 @@ const conf = JSON.parse(data);
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host: conf.host,
-  user: conf.user,
-  password: conf.password,
-  port: conf.port,
-  database: conf.database
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
 });
 connection.connect();
 
-
-// multer 로 파일명 안겹치게 해줌
 const multer = require('multer');
-const upload = multer({dest: './upload'});
+const upload = multer({dest: './upload'})
 
 app.get('/api/customers', (req, res) => {
     connection.query(
       "SELECT * FROM CUSTOMER WHERE isDeleted = 0",
       (err, rows, fields) => {
-        res.send(rows);
-        console.log(err);
-      }
+          res.send(rows);
+      }  
     );
 });
 
-// /image 경로로 접근해서, /upload 폴더에 접근
 app.use('/image', express.static('./upload'));
 
 app.post('/api/customers', upload.single('image'), (req, res) => {
-  // 첫번 째 값은 id, null 이면 알아서 들어감
-  let sql = 'INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ?, now(), 0)';
+    let sql = 'INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ?, now(), 0)';
     let image = '/image/' + req.file.filename;
     let name = req.body.name;
     let birthday = req.body.birthday;
@@ -52,17 +47,86 @@ app.post('/api/customers', upload.single('image'), (req, res) => {
             res.send(rows);
         }
     );
-})
+});
 
 app.delete('/api/customers/:id', (req, res) => {
-  let sql = 'UPDATE CUSTOMER SET isDeleted = 1 WHERE id = ?';
-  let params = [req.params.id];
-  connection.query(sql, params,
-    (err, rows, fields) => {
-      res.send(rows);
-    }
-  )
-})
+    let sql = 'UPDATE CUSTOMER SET isDeleted = 1 WHERE id = ?';
+    let params = [req.params.id];
+    connection.query(sql, params,
+        (err, rows, fields) => {
+            res.send(rows);
+        }
+    )
+});
 
-// ` 러 헤줘야 문자열 안에 변수를 잘 출력할 수 잇음
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+// const fs = require('fs');
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const app = express();
+// const port = process.env.PORT || 5000;
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true}));
+
+// const data = fs.readFileSync('./database.json');
+// const conf = JSON.parse(data);
+// const mysql = require('mysql');
+
+// const connection = mysql.createConnection({
+//   host: conf.host,
+//   user: conf.user,
+//   password: conf.password,
+//   port: conf.port,
+//   database: conf.database
+// });
+// connection.connect();
+
+
+// // multer 로 파일명 안겹치게 해줌
+// const multer = require('multer');
+// const upload = multer({dest: './upload'});
+
+// app.get('/api/customers', (req, res) => {
+//     connection.query(
+//       "SELECT * FROM CUSTOMER WHERE isDeleted = 0",
+//       (err, rows, fields) => {
+//         res.send(rows);
+//         console.log(err);
+//       }
+//     );
+// });
+
+// // /image 경로로 접근해서, /upload 폴더에 접근
+// app.use('/image', express.static('./upload'));
+
+// app.post('/api/customers', upload.single('image'), (req, res) => {
+//   // 첫번 째 값은 id, null 이면 알아서 들어감
+//   let sql = 'INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ?, now(), 0)';
+//     let image = '/image/' + req.file.filename;
+//     let name = req.body.name;
+//     let birthday = req.body.birthday;
+//     let gender = req.body.gender;
+//     let job = req.body.job;
+//     let params = [image, name, birthday, gender, job];
+//     connection.query(sql, params, 
+//         (err, rows, fields) => {
+//             res.send(rows);
+//         }
+//     );
+// })
+
+// app.delete('/api/customers/:id', (req, res) => {
+//   let sql = 'UPDATE CUSTOMER SET isDeleted = 1 WHERE id = ?';
+//   let params = [req.params.id];
+//   connection.query(sql, params,
+//     (err, rows, fields) => {
+//       res.send(rows);
+//     }
+//   )
+// })
+
+// // ` 러 헤줘야 문자열 안에 변수를 잘 출력할 수 잇음
+// app.listen(port, () => console.log(`Listening on port ${port}`));
